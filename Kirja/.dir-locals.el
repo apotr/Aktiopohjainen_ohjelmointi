f0d3dfbb5822dcc8d2d4b88efd96a706d73250cb
Heikki Virtanen <hessuvi@users.noreply.github.com>
2015-12-11 16:08:28 +0200

    Alustavat asetukset Emacsia varten.

---
+++ Kirja/.dir-locals.el
@@ -0,0 +1,8 @@
+((org-mode . ((fill-column . 1500)
+	      (org-description-max-indent . 5)
+	      (org-src-preserve-indentation . 't)
+	      (truncate-lines . nil)
+	      (mode . iimage)
+	      (mode . visual-line)
+	      ))
+ )
